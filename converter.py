ed79267471ddd147efbdd982ab8a8512fda364dd
Bruno Robert <bruno.jeanluke@gmail.com>
2021-01-27 21:07:30 -0500

    Refactored code to split the OCR functionality to external library. Updated UI to merge the convert image and convert pdf buttons into one "covert file" button.

---
+++ converter.py
@@ -0,0 +1,91 @@
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+
+
+class Converter:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def convert_image(image_path="", image_data=None):
+        """
+        Converts an image to text and returns the text gathered from an image.
+        the function can either be provided a path to the image or the image data/object itself.
+        It will return the text extracted from the image.
+        When nothing is provided, it returns an empty string. Eh!
+
+        :param image_path: image path
+        :param image_data: image data object
+        :return: string containing text extracted from the image
+        """
+        if image_data is not None:
+            return pytesseract.image_to_string(image_data)
+        elif image_path != "" and image_path is not None:
+            return pytesseract.image_to_string(Image.open(image_path))
+        else:
+            return ""
+
+    def convert_images(self, image_list) -> [str]:
+        """
+        Converts a list of image objects to text using OCR.
+        Returns a list of strings containing the text from the images in the same orders
+        as the images were passed in
+
+        :param image_list: list of image objects
+        :return: list of strings
+        """
+        text_list = []
+        for image in image_list:
+            text = self.convert_image(image_data=image)
+            text_list += [text]
+        return text_list
+
+    @staticmethod
+    def _pdf_to_images(pdf_path):
+        """
+        Converts a PDF to images and returns the array of images
+
+        :param pdf_path: The path to the pdf to convert
+        :return: the array of images
+        """
+        return convert_from_path(pdf_path, fmt='jpeg')
+
+    def convert_pdf(self, pdf_path) -> [str]:
+        """
+        Converts a pdf to text. The output is an array of text with each page of the pdf being an element of the
+        array
+
+        :param pdf_path: path to the pdf
+        :return: array of text (1 page in pdf = 1 element)
+        """
+        images = self._pdf_to_images(pdf_path)
+        pdf_pages_txt = self.convert_images(images)
+        return pdf_pages_txt
+
+    def convert_file(self, path) -> [str]:
+        """
+        Converts a file to text and returns the text in an array.
+        If the file is an image, the array will be of length 1.
+        If the file is a pdf, the array will be the same length as the number of pages (1 element per page)
+
+        :param path: path to the file to convert
+        :return: array containing text extracted from image or pdf
+        """
+        if path is None:
+            print("Not path provided!")
+            return
+        if path.lower().endswith('.pdf'):
+            return self.convert_pdf(pdf_path=path)
+        elif path.lower().endswith(('.png', '.jpg', '.jpeg')):
+            return [self.convert_image(image_path=path)]
+        else:
+            print("file type not supported")
+
+    def get_text_from_dir(self, path):
+        """
+        Converts all images in a folder to text.
+        :param path:
+        :return:
+        """
+        pass
